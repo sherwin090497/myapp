79d492c76bd756d1b4d3b5d13a7bd7af788e6fa8
Sherwin Labadan <sherwin090497@gmail.com>
2025-04-19 17:23:20 -0700

    small changes

--- app/root.tsx
+++ app/root.tsx
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-[url('/images/main_background.jpg')] bg-cover bg-no-repeat bg-center min-h-screen">
         {children}
         <ScrollRestoration />
         <Scripts />
